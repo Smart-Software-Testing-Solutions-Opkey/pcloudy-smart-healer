cf084db38c9d383ab3ec3e0efad494464c8ce966
hungerarray <ranjan.ojha01@gmail.com>
2025-09-23 16:35:24 +0545

    add: stub for page retreival from db

---
+++ smarthealer/retrieval/pageretriever.go
@@ -0,0 +1,78 @@
+package retrieval
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/Smart-Software-Testing-Solutions-Opkey/pcloudy-smart-healer/smarthealer/platform"
+	"github.com/Smart-Software-Testing-Solutions-Opkey/pcloudy-smart-healer/smarthealer/store"
+)
+
+type ComparisionMode int
+
+const (
+	AutomaticComparisionMode ComparisionMode = iota
+	ManualComparisionMode
+	ScreenshotComparisionMode
+)
+
+var ErrRetrievalFailed = errors.New("failed to retrieve candidate pages")
+
+type PageRetriever struct {
+	pageStore store.PageStore
+}
+
+func NewPageRetriever(pageStore store.PageStore) *PageRetriever {
+	return &PageRetriever{
+		pageStore: pageStore,
+	}
+}
+
+type RetereivalOptions struct {
+	ContextId string
+	B64Png    string
+	ProjectId string
+	Locator   string
+	Platform  platform.Platform
+}
+
+func (p *PageRetriever) RetrieveCandidatePages(ctx context.Context, opt RetereivalOptions, mode ComparisionMode) (int, error) {
+	if mode == AutomaticComparisionMode {
+		switch opt.Platform {
+		case platform.IosPlatform:
+			mode = ScreenshotComparisionMode
+		case platform.AndroidPlatform, platform.WebPlatform:
+			mode = ManualComparisionMode
+		default:
+			return 0, fmt.Errorf("invalid platform specified: %w", ErrRetrievalFailed)
+		}
+	}
+
+	switch mode {
+	case AutomaticComparisionMode:
+		panic("RetrieveCandidatePages invalid state")
+	case ManualComparisionMode:
+		r, err := p.usingContextID(ctx, opt)
+		if err != nil {
+			err = fmt.Errorf("%w: %w", err, ErrRetrievalFailed)
+		}
+		return r, err
+	case ScreenshotComparisionMode:
+		r, err := p.usingScreenShot(ctx, opt)
+		if err != nil {
+			err = fmt.Errorf("%w: %w", err, ErrRetrievalFailed)
+		}
+		return r, err
+	default:
+		return 0, fmt.Errorf("invalid comparision mode provided: %w", ErrRetrievalFailed)
+	}
+}
+
+func (p *PageRetriever) usingContextID(ctx context.Context, opt RetereivalOptions) (int, error) {
+	return 0, nil
+}
+
+func (p *PageRetriever) usingScreenShot(ctx context.Context, opt RetereivalOptions) (int, error) {
+	return 0, nil
+}
